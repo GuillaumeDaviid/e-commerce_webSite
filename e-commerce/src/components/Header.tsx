ab4301edef3ff5f2696bd96fbb2eb466c80d23ab
Guillaume David <guillaumedaavidpro@gmail.com>
2022-05-02 16:59:27 +0200

    add Header component

---
+++ e-commerce/src/components/Header.tsx
@@ -0,0 +1,13 @@
+function Header () {
+    return(
+        <div>
+            <h1>Commercia</h1>
+
+            <h3>Produits</h3>
+            <h3>Pack</h3>
+            <h3>Panier</h3>
+        </div>
+    )
+}
+
+export default Header
