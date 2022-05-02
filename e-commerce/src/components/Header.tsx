5c044c21ac182aafc477f6fd8b6082cf83aaa773
Guillaume David <guillaumedaavidpro@gmail.com>
2022-05-02 17:12:40 +0200

    header style

--- e-commerce/src/components/Header.tsx
+++ e-commerce/src/components/Header.tsx
@@ -1,11 +1,19 @@
+import '../styles/Header.scss'
+
 function Header () {
     return(
-        <div>
+        <div className="header">
+            <div>
             <h1>Commercia</h1>
+            </div>
 
+
+            <div className="header__link">
             <h3>Produits</h3>
             <h3>Pack</h3>
             <h3>Panier</h3>
+            </div>
+
         </div>
     )
 }
