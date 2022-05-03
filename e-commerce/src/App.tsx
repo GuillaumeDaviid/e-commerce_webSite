f6889eceead160d74f316fa1673ad358aeaacfdf
Guillaume David <guillaumedaavidpro@gmail.com>
2022-05-03 13:59:04 +0200

    react router integration

--- e-commerce/src/App.tsx
+++ e-commerce/src/App.tsx
@@ -3,12 +3,15 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './components/Home'
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />    
-      <Home />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes> 
       <Footer />
     </div>
   );
