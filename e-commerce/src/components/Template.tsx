49dc146cd768d0c3bf9d975df23f2bb0a5428b52
Guillaume David <guillaumedaavidpro@gmail.com>
2022-05-04 11:52:16 +0200

    create template component

---
+++ e-commerce/src/components/Template.tsx
@@ -0,0 +1,11 @@
+import { productList } from '../datas/productList'
+
+function Template (category:string) {
+    return(
+        <div className='Template'>
+            si category = vetement alors affiche ... photo, nom, prix, stock ... 
+        </div>
+    )
+}
+
+export default Template
