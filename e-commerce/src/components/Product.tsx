0001fb4a8dd67940bfc2f8c061f608f21ded67ee
Guillaume David <guillaumedaavidpro@gmail.com>
2022-05-09 11:38:37 +0200

    style product component

--- e-commerce/src/components/Product.tsx
+++ e-commerce/src/components/Product.tsx
@@ -5,13 +5,11 @@ import Template from './Template'
 function Product () {
     let { id } = useParams()
 
-    console.log(id?.substring(3));
-
     const title = id === "id=vetement" ? <h1>Vetements</h1> : id === "id=informatique" ? <h1>Informatique</h1> : id === "id=sport" ? <h1>Sport</h1> 
     : id === "id=bricolage" ? <h1>Bricolage</h1> : id === "id=plante" ? <h1>Plantes</h1> : <div></div>
     return(
         <div className="Product">
-            <Link to="/">retour</Link>
+            <Link to="/" className='Product_back'>retour</Link>
             {title}
             <Template category={`${id?.substring(3)}`}/>
         </div>
