8176e1ec511330bd5ede125d823a3d3dd13dea36
Guillaume David <guillaumedaavidpro@gmail.com>
2022-05-11 11:04:51 +0200

    create Detail.scss file

--- e-commerce/src/components/Detail.tsx
+++ e-commerce/src/components/Detail.tsx
@@ -1,12 +1,13 @@
 import {useParams, Link} from "react-router-dom";
 import { productList } from '../datas/productList'
 import img from "../images/running.jpg"
+import '../styles/Detail.scss'
 
 function Detail () {
     let {id} = useParams()
 
     const item = productList.map((data) => 
-    id?.substring(3) === data.id ? <div className='Template__item'><img  className='Detail_item-img' src={img} alt={data.name}/>
+    id?.substring(3) === data.id ? <div className='Template__item'><img  className='detail_item-img' src={img} alt={data.name}/>
     <p className='item-name'>{data.name}</p>
     <p className='item-price'>{data.price} €</p>
     </div> : <div></div>)
