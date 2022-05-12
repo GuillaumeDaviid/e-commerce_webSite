71713d487f96093454eae6d445aa777219880a85
Guillaume David <guillaumedaavidpro@gmail.com>
2022-05-12 11:02:34 +0200

    useStore and dispatch in components + wrapped provider

--- e-commerce/src/components/Detail.tsx
+++ e-commerce/src/components/Detail.tsx
@@ -1,16 +1,25 @@
 import {useParams} from "react-router-dom";
 import { productList } from '../datas/productList'
-import img from "../images/running.jpg"
+import { useDispatch, useStore, useSelector } from 'react-redux';
 import '../styles/Detail.scss'
+import { changeTotal } from "../redux/store";
 
 function Detail () {
+    const store = useStore();
     let {id} = useParams()
+
+    const dispatch = useDispatch();
+    const totalPrice = useSelector((state:any) => state.totalPrice);
+
+    console.log(totalPrice);
 
     const item = productList.map((data) => 
     id?.substring(3) === data.id ? <div className='Template__item'><img  className='Detail_item-img' src={data.image} alt={data.name}/>
     <p className='Detail_item-name'>{data.name}</p>
     <p className='Detail_item-price'>{data.price} €</p>
-    <button className="Detail-btn">Ajouter au Panier</button>
+    <button className="Detail-btn" onClick={() => {
+        store.dispatch(changeTotal())
+    }}>Ajouter au Panier</button>
     </div> : <div></div>)
 
     return (
--- e-commerce/src/redux/store.ts
+++ e-commerce/src/redux/store.ts
@@ -1,4 +1,7 @@
-import { createStore, combineReducers } from "redux";
+import { combineReducers } from "redux";
+import { legacy_createStore as createStore} from 'redux'
+
+const reduxDevtools = (window as any)['__REDUX_DEVTOOLS_EXTENSION__']  && (window as any)['__REDUX_DEVTOOLS_EXTENSION__()']
 
 export const changeTotal = () => ({ type: "changeTotal"});
 
@@ -6,29 +9,33 @@ export const changeQuantity = () => ({ type: "changeQuantity"});
 
 export const addCommand = () => ({ type: "addCommand"});
 
+
 const totalPriceReducer = (state:number = 0, action:any) => {
     if (action.type === "changeTotal"){
         return state + 1;
     }
+    return state;
 }
 
 const quantityReducer = (state:number = 0, action:any) => {
     if (action.type === "changeQuantity"){
         return state + 1
     }
+    return state;
 }
 
 const commandReducer = (state:any[] = [], action:any) =>  {
     if (action.type === "addCommand"){
         return state.push('ballon');
     }
+    return state;
 }
 
 
 const reducer = combineReducers({
     totalPrice: totalPriceReducer,
     quantity: quantityReducer,
-    command: combineReducers,
+    command: commandReducer,
   });
   
-  export const store = createStore(reducer);
+export const store = createStore(reducer, reduxDevtools);
