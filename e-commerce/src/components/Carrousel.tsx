db93bbd1f91cfc69d2bb6d0a5bbaabfffb4244d6
Guillaume David <guillaumedaavidpro@gmail.com>
2022-05-19 11:59:19 +0200

    add image + style carrousel

--- e-commerce/src/components/Carrousel.tsx
+++ e-commerce/src/components/Carrousel.tsx
@@ -1,7 +1,16 @@
+import adidas from '../images/adidas.jpg'
+import chaussures from '../images/chaussures.png'
+import sport from '../images/sport.jpg'
+import '../styles/Carrousel.scss'
+
 function Carrousel () {
     return (
-        <div>
-            image
+        <div className="carrousel">
+            <img className='carrousel_image-item' src={adidas} alt="sport"/>
+            <img className='carrousel_image-item' src={chaussures} alt="sport"/>
+            <img className='carrousel_image-item' src={sport} alt="sport"/>
+            <div className='carrousel_right'></div>
+            <div className='carrousel_left'></div>
         </div>
     )
 }
