8a9e4d91e45b503040eac80756b7075c8c2dca01
Guillaume David <guillaumedaavidpro@gmail.com>
2022-05-19 12:17:51 +0200

    style btn carrousel

--- e-commerce/src/components/Carrousel.tsx
+++ e-commerce/src/components/Carrousel.tsx
@@ -1,6 +1,7 @@
 import adidas from '../images/adidas.jpg'
 import chaussures from '../images/chaussures.png'
 import sport from '../images/sport.jpg'
+import chevron from '../images/chevron.png'
 import '../styles/Carrousel.scss'
 
 function Carrousel () {
@@ -9,8 +10,8 @@ function Carrousel () {
             <img className='carrousel_image-item' src={adidas} alt="sport"/>
             <img className='carrousel_image-item' src={chaussures} alt="sport"/>
             <img className='carrousel_image-item' src={sport} alt="sport"/>
-            <div className='carrousel_right'></div>
-            <div className='carrousel_left'></div>
+            <div className='carrousel_right'><img  className='chevron-right' src={chevron} alt="chevron"/></div>
+            <div className='carrousel_left'><img src={chevron} alt="chevron"/></div>
         </div>
     )
 }
