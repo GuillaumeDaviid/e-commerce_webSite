56c755b41bc44365b0ff182af2de1dd2cae21569
Guillaume David <guillaumedaavidpro@gmail.com>
2022-05-27 10:00:17 +0200

    test: Header component

--- e-commerce/src/test/header.test.tsx
+++ e-commerce/src/test/header.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header'
 import { BrowserRouter as Router } from "react-router-dom";
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 
 describe('Header', () => {
@@ -11,5 +11,38 @@ describe('Header', () => {
             <Header/>
             </Router>
         );
+    })
+
+    test('should render title', async () => {
+        render(
+            <Router>
+            <Header/>
+            </Router>
+        );
+        const titleElement = screen.getByTestId('website_title');
+        expect(titleElement).toBeInTheDocument();
+        expect(titleElement).toHaveTextContent("COMMERCIA");
+        expect(titleElement).toHaveAttribute("data-testid", "website_title");
+    })
+
+    test('should have nav elements', async () => {
+        render(
+            <Router>
+            <Header/>
+            </Router>
+        );
+        const navElement = screen.getAllByTestId('nav_element');
+        expect(navElement[0]).toHaveTextContent("Vetements");
+        expect(navElement[0]).toHaveAttribute("href", "/product/id=vetement");
+    })
+
+    test('should have cart link', async () => {
+        render(
+            <Router>
+            <Header/>
+            </Router>
+        );
+        const cartLink = screen.getByTestId('cartLink');
+        expect(cartLink).toHaveAttribute("href", "/cart");
     })
 })
